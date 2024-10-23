ffeb2ed8c30bf2a86e51fb4c0db19d66c0cb667a
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-23 09:40:11 -0400

    Add project files.

---
+++ Topic 10 - Assignment/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Topic_10___Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPenguins = new System.Windows.Forms.Button();
+            this.btnKraken = new System.Windows.Forms.Button();
+            this.btnLeafs = new System.Windows.Forms.Button();
+            this.btnKings = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.imgLogos = new System.Windows.Forms.PictureBox();
+            this.btnBlackhawks = new System.Windows.Forms.Button();
+            this.btnStars = new System.Windows.Forms.Button();
+            this.btnBruins = new System.Windows.Forms.Button();
+            this.btnDevils = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPenguins
+            // 
+            this.btnPenguins.Location = new System.Drawing.Point(12, 55);
+            this.btnPenguins.Name = "btnPenguins";
+            this.btnPenguins.Size = new System.Drawing.Size(123, 41);
+            this.btnPenguins.TabIndex = 0;
+            this.btnPenguins.Text = "Penguins";
+            this.btnPenguins.UseVisualStyleBackColor = true;
+            this.btnPenguins.MouseHover += new System.EventHandler(this.btnPenguins_MouseHover);
+            // 
+            // btnKraken
+            // 
+            this.btnKraken.Location = new System.Drawing.Point(12, 118);
+            this.btnKraken.Name = "btnKraken";
+            this.btnKraken.Size = new System.Drawing.Size(123, 41);
+            this.btnKraken.TabIndex = 1;
+            this.btnKraken.Text = "Kracken";
+            this.btnKraken.UseVisualStyleBackColor = true;
+            this.btnKraken.MouseHover += new System.EventHandler(this.btnKraken_MouseHover);
+            // 
+            // btnLeafs
+            // 
+            this.btnLeafs.Location = new System.Drawing.Point(12, 181);
+            this.btnLeafs.Name = "btnLeafs";
+            this.btnLeafs.Size = new System.Drawing.Size(123, 41);
+            this.btnLeafs.TabIndex = 2;
+            this.btnLeafs.Text = "Leafs";
+            this.btnLeafs.UseVisualStyleBackColor = true;
+            this.btnLeafs.MouseHover += new System.EventHandler(this.btnLeafs_MouseHover);
+            // 
+            // btnKings
+            // 
+            this.btnKings.Location = new System.Drawing.Point(12, 243);
+            this.btnKings.Name = "btnKings";
+            this.btnKings.Size = new System.Drawing.Size(123, 41);
+            this.btnKings.TabIndex = 3;
+            this.btnKings.Text = "Kings";
+            this.btnKings.UseVisualStyleBackColor = true;
+            this.btnKings.MouseHover += new System.EventHandler(this.btnKings_MouseHover);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 311);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(123, 41);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.Black;
+            this.btnQuit.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.ForeColor = System.Drawing.Color.White;
+            this.btnQuit.Location = new System.Drawing.Point(166, 311);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(123, 41);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // imgLogos
+            // 
+            this.imgLogos.Location = new System.Drawing.Point(326, 55);
+            this.imgLogos.Name = "imgLogos";
+            this.imgLogos.Size = new System.Drawing.Size(453, 297);
+            this.imgLogos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgLogos.TabIndex = 6;
+            this.imgLogos.TabStop = false;
+            this.imgLogos.Visible = false;
+            // 
+            // btnBlackhawks
+            // 
+            this.btnBlackhawks.Location = new System.Drawing.Point(166, 243);
+            this.btnBlackhawks.Name = "btnBlackhawks";
+            this.btnBlackhawks.Size = new System.Drawing.Size(123, 41);
+            this.btnBlackhawks.TabIndex = 7;
+            this.btnBlackhawks.Text = "Blackhawks";
+            this.btnBlackhawks.UseVisualStyleBackColor = true;
+            this.btnBlackhawks.MouseHover += new System.EventHandler(this.btnBlackhawks_MouseHover);
+            // 
+            // btnStars
+            // 
+            this.btnStars.Location = new System.Drawing.Point(166, 55);
+            this.btnStars.Name = "btnStars";
+            this.btnStars.Size = new System.Drawing.Size(123, 41);
+            this.btnStars.TabIndex = 8;
+            this.btnStars.Text = "Stars";
+            this.btnStars.UseVisualStyleBackColor = true;
+            this.btnStars.MouseHover += new System.EventHandler(this.btnStars_MouseHover);
+            // 
+            // btnBruins
+            // 
+            this.btnBruins.Location = new System.Drawing.Point(166, 118);
+            this.btnBruins.Name = "btnBruins";
+            this.btnBruins.Size = new System.Drawing.Size(123, 41);
+            this.btnBruins.TabIndex = 9;
+            this.btnBruins.Text = "Bruins";
+            this.btnBruins.UseVisualStyleBackColor = true;
+            this.btnBruins.MouseHover += new System.EventHandler(this.btnBruins_MouseHover);
+            // 
+            // btnDevils
+            // 
+            this.btnDevils.Location = new System.Drawing.Point(166, 181);
+            this.btnDevils.Name = "btnDevils";
+            this.btnDevils.Size = new System.Drawing.Size(123, 41);
+            this.btnDevils.TabIndex = 10;
+            this.btnDevils.Text = "Devils";
+            this.btnDevils.UseVisualStyleBackColor = true;
+            this.btnDevils.MouseHover += new System.EventHandler(this.btnDevils_MouseHover);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(19, 0);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(760, 39);
+            this.lblInstructions.TabIndex = 11;
+            this.lblInstructions.Text = "Please Select A Hockey Team To Learn More";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(12, 374);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(66, 24);
+            this.lblInfo.TabIndex = 12;
+            this.lblInfo.Text = "label1";
+            this.lblInfo.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnDevils);
+            this.Controls.Add(this.btnBruins);
+            this.Controls.Add(this.btnStars);
+            this.Controls.Add(this.btnBlackhawks);
+            this.Controls.Add(this.imgLogos);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnKings);
+            this.Controls.Add(this.btnLeafs);
+            this.Controls.Add(this.btnKraken);
+            this.Controls.Add(this.btnPenguins);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPenguins;
+        private System.Windows.Forms.Button btnKraken;
+        private System.Windows.Forms.Button btnLeafs;
+        private System.Windows.Forms.Button btnKings;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox imgLogos;
+        private System.Windows.Forms.Button btnBlackhawks;
+        private System.Windows.Forms.Button btnStars;
+        private System.Windows.Forms.Button btnBruins;
+        private System.Windows.Forms.Button btnDevils;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Topic 10 - Assignment/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_10___Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPenguins_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.pensLogo;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Pitsburgh Penguins are an NHL team in the Metropolitain Division. " +
+                "They have won 5 Stanley Cups.";
+        }
+
+        private void btnStars_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.starsLogo;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Dallas Stars are an NHL team in the Central Division. " +
+                "They have won 1 Stanley Cup.";
+        }
+
+        private void btnBruins_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.bruinsLogo;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Boston Bruins are an NHL team in the Atlantic Division. " +
+                "They have won 6 Stanley Cups.";
+        }
+
+        private void btnKraken_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.krakenLogo_2_;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Seatle Kraken are an NHL team in the Pacific Division. " +
+                "They are the newest NHL franchise.";
+        }
+
+        private void btnLeafs_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.leafsLogo_1_;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Toronto Maple Leafs are an NHL team in the Atlantic Division. " +
+                "They have won 13 Stanley Cups.";
+        }
+
+        private void btnDevils_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.devilsLogo;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The New Jersey Devils are an NHL team in the Metropolitain Division. " +
+                "They have won 3 Stanley Cups.";
+        }
+
+        private void btnKings_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.kingsLogo_1_;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Los Angeles Kings are an NHL team in the Pacific Division. " +
+                "They have won 2 Stanley Cups.";
+        }
+
+        private void btnBlackhawks_MouseHover(object sender, EventArgs e)
+        {
+            imgLogos.Visible = true;
+            imgLogos.Image = Properties.Resources.blackhawksLogo;
+            lblInfo.Visible = true;
+            lblInfo.Text = "The Boston Bruins are an NHL team in the Central Division. " +
+                "They have won 6 Stanley Cups.";
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            imgLogos.Visible = false;
+            lblInfo.Visible = false;
+        }
+    }
+}
